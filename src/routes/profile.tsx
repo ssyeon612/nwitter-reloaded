a81c5229f7c63103f4a8e3d04bf1e7c87c730220
ssyeon612 <ssyeon612@gmai.com>
2023-11-26 22:00:07 +0900

    feat: 사용자 닉네임 수정 기능 추가

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
-import { auth, storage } from "../firebase";
-import { useState } from "react";
+import { auth, db, storage } from "../firebase";
+import { useEffect, useState } from "react";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { updateProfile } from "firebase/auth";
+import { collection, getDocs, limit, orderBy, query, where } from "firebase/firestore";
+import { ITweet } from "../components/timeline";
+import Tweet from "../components/tweets";
 
 const Wrapper = styled.div`
     display: flex;
@@ -34,13 +37,52 @@ const AvatarInput = styled.input`
     display: none;
 `;
 
+const NameBox = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+`;
+
 const Name = styled.span`
     font-size: 22px;
+`;
+
+const NameInput = styled.input`
+    font-size: 22px;
+    background-color: white;
+    color: black;
+    border-radius: 2px;
+    &:focus {
+        outline: none;
+        border-color: #1d9bf0;
+    }
+`;
+
+const EditButton = styled.button`
+    background-color: transparent;
+    border: 0;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    svg {
+        width: 20px;
+        fill: white;
+    }
+`;
+
+const Tweets = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
 `;
 
 export default function Profile() {
     const user = auth.currentUser;
     const [avatar, setAvatar] = useState(user?.photoURL);
+    const [isEdit, setIsEdit] = useState(false);
+    const [name, setName] = useState(user?.displayName);
+    const [tweets, setTweets] = useState<ITweet[]>([]);
     const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
         const { files } = e.target;
         if (!user) return;
@@ -53,6 +95,39 @@ export default function Profile() {
             await updateProfile(user, { photoURL: avatarUrl });
         }
     };
+
+    const onEditName = async () => {
+        setIsEdit(!isEdit);
+        if (isEdit) {
+            if (!user || name === "") return;
+            await updateProfile(user, { displayName: name });
+        }
+    };
+
+    const onChangeName = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setName(value);
+    };
+
+    const fetchTweets = async () => {
+        const tweetQuery = query(collection(db, "tweets"), where("userId", "==", user?.uid), orderBy("createAt", "desc"), limit(25));
+        const snapshot = await getDocs(tweetQuery);
+        const tweets = snapshot.docs.map((doc) => {
+            const { tweet, createAt, userId, username, photo } = doc.data();
+            return {
+                tweet,
+                createAt,
+                userId,
+                username,
+                photo,
+                id: doc.id,
+            };
+        });
+        setTweets(tweets);
+    };
+    useEffect(() => {
+        fetchTweets();
+    }, []);
 
     return (
         <Wrapper>
@@ -70,7 +145,20 @@ export default function Profile() {
                 )}
             </AvatarUpload>
             <AvatarInput onChange={onAvatarChange} id="avatar" type="file" accept="image/*" />
-            <Name>{user?.displayName ?? "익명"}</Name>
+            {/* 이름 수정 */}
+            <NameBox>
+                {isEdit ? <NameInput onChange={onChangeName} type="text" value={name ? name : ""} /> : <Name>{name ?? "익명"}</Name>}
+                <EditButton onClick={onEditName}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                        <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
+                    </svg>
+                </EditButton>
+            </NameBox>
+            <Tweets>
+                {tweets.map((tweet) => (
+                    <Tweet key={tweet.id} {...tweet} />
+                ))}
+            </Tweets>
         </Wrapper>
     );
 }
